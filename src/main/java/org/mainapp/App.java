f1b7604f1892e2255c2f8a0a7044ba7658b4f3ec
aurelionsulll <mennanamehdi@gmail.com>
2021-03-19 12:25:13 +0100

    mvc

--- src/main/java/org/mainapp/App.java
+++ src/main/java/org/mainapp/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("mainapp/login.fxml"), 640, 480);
+        scene = new Scene(loadFXML("login"), 640, 480);
         stage.setScene(scene);
         stage.show();
 //        LoginController log = new LoginController();
