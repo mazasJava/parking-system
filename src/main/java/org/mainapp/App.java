e8ee1dbee64c822113277aa8f1864f95f19aa3f4
Abdelfattah Bouhlali <abdelfattah.bouhlali99@hotmail.com>
2021-04-24 22:48:30 +0200

    Revert "Update LoginController and login.fxml with validation of input fields"
    This reverts commit 342051d12d8a2b738a04b8f79a1b18de547e2d55.

--- src/main/java/org/mainapp/App.java
+++ src/main/java/org/mainapp/App.java
@@ -43,7 +43,7 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-         //DbConnection.connect();
+         DbConnection.connect();
         launch();
     }
 
