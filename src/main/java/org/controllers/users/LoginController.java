6d22749c2da5e818c15d56bbd12c1a7915d5cd25
Abdelfattah Bouhlali <abdelfattah.bouhlali99@hotmail.com>
2021-04-24 22:49:56 +0200

    Revert "Revert "Update LoginController and login.fxml with validation of input fields""
    This reverts commit e8ee1dbee64c822113277aa8f1864f95f19aa3f4.

--- src/main/java/org/controllers/users/LoginController.java
+++ src/main/java/org/controllers/users/LoginController.java
@@ -1,73 +1,112 @@
 package org.controllers.users;
 
 import com.mongodb.*;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.ProgressIndicator;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import org.controllers.DbConnection;
 import org.mainapp.App;
-
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+
 public class LoginController implements Initializable {
-    public MongoClient mongoClient;
     @FXML
     public TextField usernameTextField;
     @FXML
     public PasswordField passwordTextField;
     @FXML
     public Label labelError;
-
     @FXML
     ProgressIndicator logProg;
 
     @FXML
     public void switchToCar() throws IOException {
         App.setRoot("car");
-    }
 
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        logProg.setVisible(false);
+
+    }
+    public void alert(String title,String message,String header){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.setHeaderText(header);
+        alert.showAndWait();
+    }
+    public void createCount(MouseEvent mouseEvent) throws IOException {
+        //App.setRoot("register");
     }
     class getUserTask extends Task<Integer> {
-
         @Override
         protected Integer call() throws Exception {
-
-            updateProgress(0,10);
-            Thread.sleep(500);
+            Thread thread = new Thread();
+            updateProgress(4,10);
             try {
                 if (!usernameTextField.getText().equals("") && !passwordTextField.getText().equals("")){
                     logProg.setVisible(true);
-                    System.out.println("user: " + usernameTextField.getText() + ", mdp: " + passwordTextField.getText());
+                    updateProgress(2,10);
+                    thread.sleep(250);
+                    DbConnection.connect();
+                    updateProgress(4,10);
+                    MongoDatabase database = DbConnection.database;
                     updateProgress(5,10);
-                    Thread.sleep(500);
-                    //put the script to fetch database here
+                    MongoCollection<BasicDBObject> col = database.getCollection("users",BasicDBObject.class);
+                    updateProgress(6,10);
+                    List<BasicDBObject> fd = col.find().into(new ArrayList<BasicDBObject>());
+                    updateProgress(7,10);
+                    boolean tr = false;
 
-                    if(usernameTextField.getText().equals("admin") && passwordTextField.getText().equals("admin")){
+                    for (BasicDBObject bo : fd){
+                        if(usernameTextField.getText().equals(bo.get("name")) && passwordTextField.getText().equals(bo.get("password"))){
+                            updateProgress(8,10);
+                            thread.sleep(250);
+                            updateProgress(10,10);
+                            try{
+                                switchToCar();
+                            }catch(Exception e ){
+                                System.out.println("eeexception"+e.getMessage());
+                            }
+                            return 1;
+                        }
+                        updateProgress(9,10);
+                    }
+                    if(tr == false){
+                        updateProgress(8,10);
+                        thread.sleep(250);
                         updateProgress(10,10);
-                        Thread.sleep(500);
-                        switchToCar();
-                    }else{
-                        updateProgress(10,10);
-                        Thread.sleep(500);
-                        System.out.println("Aucun utisateur corresponde avec ce login!!!");
                         logProg.setVisible(false);
+                        Platform.runLater(() -> {
+                            alert("WARNING","EMAIL OR PASSWORD IS INCORRECT!","LOGIN ERROR");
+                        });
                     }
                 }else{
-                    System.out.println("Inputs vides!!!");
+                    updateProgress(10,10);
                     logProg.setVisible(false);
+                    Platform.runLater(() -> {
+                        alert("WARNING","EMAIL OR PASSWORD IS EMPTY","LOGIN ERROR");
+                    });
                 }
-            }catch(Exception e) {}
+            }catch (Exception e) {
+                System.out.println("exception"+e.getMessage());
+            }
+            finally {
+                updateProgress(10,10);
+                logProg.setVisible(false);
+                System.out.println("finally");
+            }
             return 1;
         }
 
@@ -76,46 +115,22 @@ public class LoginController implements Initializable {
             updateMessage("Cancelled!!");
             return super.cancel(mayInterruptIfRunning);
         }
-
         @Override
         protected void updateProgress(double workDone, double max) {
             updateMessage("progresse! "+ workDone);
             super.updateProgress(workDone, max);
         }
     }
-
-
     @FXML
     public void login() throws IOException {
 
         getUserTask task = new getUserTask();
         logProg.progressProperty().bind(task.progressProperty());
-        new Thread(task).start();
+        new Thread(task).start();    }
 
-        //DbConnection.connect();
-        //@Deprecated
-        //DB db = mongoClient.getDB("PARKING_MANAGEMENT_SYSTEM");
-        //DBCollection collection = db.getCollection("users");
-        //checkAuth(collection);
-    }
-
-    public void checkAuth(DBCollection collection) throws IOException {
-        BasicDBObject whereQuery1 = new BasicDBObject();
-        BasicDBObject whereQuery2 = new BasicDBObject();
-        whereQuery1.put("name", usernameTextField.getText());
-        whereQuery2.put("password", passwordTextField.getText());
-        DBCursor cursor = collection.find(whereQuery1, whereQuery2);
-        if (cursor.hasNext()) {
-            System.out.println(cursor.next());
-            switchToCar();
-        } else {
-            labelError.setText("username or password is incorrect");
-        }
-
-    }
-
-    // Add focus Enter . 
+    // Add focus Enter .
     public void clickEnterUserName(ActionEvent actionEvent) {
         passwordTextField.requestFocus();
     }
+
 }
