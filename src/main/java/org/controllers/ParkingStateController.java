00f700f9b3441f2eed617fbdef48944b0d6d9bcf
BABDELADIM <abdeladim.belhaous@gmail.com>
2021-04-29 17:59:26 +0000

    update controller ParkingStateController.java

--- src/main/java/org/controllers/ParkingStateController.java
+++ src/main/java/org/controllers/ParkingStateController.java
@@ -1,7 +1,10 @@
 package org.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
 import org.mainapp.App;
 
 import java.io.IOException;
@@ -9,17 +12,25 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ParkingStateController implements Initializable {
-//    @FXML
-//    public void switchToParkingCar() throws IOException {
-//        App.setRoot("car");
-//    }
-//    @FXML
-//    public void switchToParkingState() throws IOException {
-//        App.setRoot("parkingState");
-//    }
+
+
+    @FXML
+    private PieChart pieChart;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        iniPieChart();
+
+    }
+
+
+
+    public void iniPieChart(){
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+                new PieChart.Data("free",14),
+                new PieChart.Data("full",36)
+        );
+        pieChart.setData(pieChartData);
     }
 }
