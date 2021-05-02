8b16a8d7b9fd33cb9cafcaf93e436a71c30c7998
aurelionsulll <mennanamehdi@gmail.com>
2021-05-02 22:26:45 +0000

    client model

---
+++ src/main/java/org/models/Client.java
@@ -0,0 +1,89 @@
+package org.models;
+
+public class Client {
+
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+    private String carPlate;
+
+    public Client() {
+    }
+
+    public Client(int id, String name, String email, String phone, String password, String carPlate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.carPlate = carPlate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Client setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Client setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Client setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public Client setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Client setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getCarPlate() {
+        return carPlate;
+    }
+
+    public Client setCarPlate(String carPlate) {
+        this.carPlate = carPlate;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", password='" + password + '\'' +
+                ", carPlate='" + carPlate + '\'' +
+                '}';
+    }
+}
