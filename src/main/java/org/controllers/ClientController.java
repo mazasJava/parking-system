370d932d30871fc7d969493d6ae46389b326c831
aurelionsulll <mennanamehdi@gmail.com>
2021-05-03 02:48:56 +0000

    limit page size

--- src/main/java/org/controllers/ClientController.java
+++ src/main/java/org/controllers/ClientController.java
@@ -3,25 +3,24 @@ package org.controllers;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Sorts;
 import org.models.Client;
-import org.models.History;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class ClientController {
 
-    public static List<Client> getClientList()
-    {
+
+    public static List<Client> getClientsList(int pageSize) {
         MongoCollection<Client> clientMongoCollection = DbConnection.database.getCollection("clients", Client.class);
-        List<Client> clients = clientMongoCollection.find().sort(Sorts.ascending("timeStamp")).into(new ArrayList<>());
-//        List<Client> clients = clientMongoCollection.find().into(new ArrayList<>());
+        List<Client> clients = clientMongoCollection.find()
+                .sort(Sorts.ascending("timeStamp"))
+                .limit(pageSize).into(new ArrayList<>());
         return clients;
     }
 
     public static void main(String[] args) {
         DbConnection.connect();
-        System.out.println(getClientList());
+        System.out.println(getClientsList(6));
     }
 
 }
