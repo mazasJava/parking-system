33210595a478c96a536aef3f2da4fa3bea499005
BABDELADIM <abdeladim.belhaous@gmail.com>
2021-05-03 16:09:23 +0000

    update DashboardController.java

--- src/main/java/org/controllers/DashboardController.java
+++ src/main/java/org/controllers/DashboardController.java
@@ -20,49 +20,94 @@ public class DashboardController implements Initializable {
 //    @FXML
 //    private URL location;
 
-    @FXML
-    private PieChart pieChart;
+
+
+
 
     @FXML
-    private BarChart<?, ?> visitchart;
+    private CategoryAxis x= new CategoryAxis();
 
     @FXML
-    private CategoryAxis x;
+    private NumberAxis y = new NumberAxis();
 
     @FXML
-    private NumberAxis y;
-
+    LineChart<String,Number> lineChart = new LineChart<String,Number>(x,y);
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Parking park = new Parking("",10);
-        Parking.addCar(new Car());
-        Parking.addCar(new Car());
-        Parking.addCar(new Car());
-        Parking.addCar(new Car());
-        iniPieChart(Parking.getState()[1], Parking.getState()[2]);
+        iniLineChart();
     }
 
-    private void iniBarChart(){
+    private void iniLineChart(){
 
-        XYChart.Series set1 = new XYChart.Series();
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("Park A");
 
-        set1.getData().add(new XYChart.Data("Monday",44));
-        set1.getData().add(new XYChart.Data("Tuesday",36));
-        set1.getData().add(new XYChart.Data("Wednesday",20));
-        set1.getData().add(new XYChart.Data("Thursday",15));
-        set1.getData().add(new XYChart.Data("Friday",13));
-        set1.getData().add(new XYChart.Data("Saturday",50));
-        set1.getData().add(new XYChart.Data("Sunday",53));
-        visitchart.getData().addAll(set1);
+        series1.getData().add(new XYChart.Data("Jan", 23));
+        series1.getData().add(new XYChart.Data("Feb", 14));
+        series1.getData().add(new XYChart.Data("Mar", 15));
+        series1.getData().add(new XYChart.Data("Apr", 24));
+        series1.getData().add(new XYChart.Data("May", 34));
+        series1.getData().add(new XYChart.Data("Jun", 36));
+        series1.getData().add(new XYChart.Data("Jul", 22));
+        series1.getData().add(new XYChart.Data("Aug", 45));
+        series1.getData().add(new XYChart.Data("Sep", 43));
+        series1.getData().add(new XYChart.Data("Oct", 17));
+        series1.getData().add(new XYChart.Data("Nov", 29));
+        series1.getData().add(new XYChart.Data("Dec", 25));
+
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("PARK B");
+        series2.getData().add(new XYChart.Data("Jan", 33));
+        series2.getData().add(new XYChart.Data("Feb", 34));
+        series2.getData().add(new XYChart.Data("Mar", 25));
+        series2.getData().add(new XYChart.Data("Apr", 44));
+        series2.getData().add(new XYChart.Data("May", 39));
+        series2.getData().add(new XYChart.Data("Jun", 16));
+        series2.getData().add(new XYChart.Data("Jul", 55));
+        series2.getData().add(new XYChart.Data("Aug", 54));
+        series2.getData().add(new XYChart.Data("Sep", 48));
+        series2.getData().add(new XYChart.Data("Oct", 27));
+        series2.getData().add(new XYChart.Data("Nov", 37));
+        series2.getData().add(new XYChart.Data("Dec", 29));
+
+        XYChart.Series series3 = new XYChart.Series();
+        series3.setName("PARK C");
+        series3.getData().add(new XYChart.Data("Jan", 44));
+        series3.getData().add(new XYChart.Data("Feb", 35));
+        series3.getData().add(new XYChart.Data("Mar", 36));
+        series3.getData().add(new XYChart.Data("Apr", 33));
+        series3.getData().add(new XYChart.Data("May", 31));
+        series3.getData().add(new XYChart.Data("Jun", 26));
+        series3.getData().add(new XYChart.Data("Jul", 22));
+        series3.getData().add(new XYChart.Data("Aug", 25));
+        series3.getData().add(new XYChart.Data("Sep", 43));
+        series3.getData().add(new XYChart.Data("Oct", 44));
+        series3.getData().add(new XYChart.Data("Nov", 45));
+        series3.getData().add(new XYChart.Data("Dec", 44));
+
+
+        XYChart.Series series4 = new XYChart.Series();
+        series4.setName("PARK D");
+        series4.getData().add(new XYChart.Data("Jan", 40));
+        series4.getData().add(new XYChart.Data("Feb", 38));
+        series4.getData().add(new XYChart.Data("Mar", 32));
+        series4.getData().add(new XYChart.Data("Apr", 30));
+        series4.getData().add(new XYChart.Data("May", 39));
+        series4.getData().add(new XYChart.Data("Jun", 27));
+        series4.getData().add(new XYChart.Data("Jul", 21));
+        series4.getData().add(new XYChart.Data("Aug", 27));
+        series4.getData().add(new XYChart.Data("Sep", 33));
+        series4.getData().add(new XYChart.Data("Oct", 24));
+        series4.getData().add(new XYChart.Data("Nov", 49));
+        series4.getData().add(new XYChart.Data("Dec", 54));
+
+        lineChart.getData().addAll(series1, series2, series3,series4);
+
+
+
     }
-    public void iniPieChart(int x,int y){
-        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
-                new PieChart.Data("Saturé",x),
-                new PieChart.Data("Libre",y)
-        );
-        pieChart.setData(pieChartData);
-    }
+
 
 }
