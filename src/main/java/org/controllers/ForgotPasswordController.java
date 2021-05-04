37ca0285b0e014b026d65c3c20e417ad84815849
elmarnissi-Soufiane <79916810+elmarnissi-Soufiane@users.noreply.github.com>
2021-05-04 20:25:16 +0000

    Add ForgotPasswordController and forgotPassword.fxml

---
+++ src/main/java/org/controllers/ForgotPasswordController.java
@@ -0,0 +1,24 @@
+package org.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ForgotPasswordController {
+
+    @FXML
+    TextField emailTextField;
+
+    public void ConfirmForgotPassword(ActionEvent actionEvent) {
+//        ForgotPassword forgotPassword = new ForgotPassword();
+//        try {
+//            forgotPassword.sendEmail(emailTextField.getText());
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    public void clickEnterEmail(ActionEvent actionEvent) {
+        emailTextField.requestFocus();
+    }
+}
